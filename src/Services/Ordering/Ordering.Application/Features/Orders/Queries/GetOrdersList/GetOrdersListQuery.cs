aaa411f381606fd83b28f0217bd6d417ae0eee1c
kodoku-x <datsunbae@gmail.com>
2024-03-11 00:18:49 +0700

    fixbug ordering service

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+public sealed record GetOrdersListQuery : IRequest<List<OrderResponse>>
+{
+    public string UserName { get; set; }
+
+    public GetOrdersListQuery(string userName)
+    {
+        UserName = userName;
+    }
+}
